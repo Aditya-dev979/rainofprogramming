696aca407b5fcc05b476f02e44e8c5dcc636bd09
Aditya-dev979 <adityajangir979@gmail.com>
2025-01-29 19:57:22 +0530

    change all navbar file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,166 +1,5 @@
 
-
-// import { Menu, School } from "lucide-react";
-// import React, { useEffect } from "react";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuGroup,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuSeparator,
-//   DropdownMenuTrigger,
-// } from "./ui/dropdown-menu";
-// import { Button } from "./ui/button";
-// import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-// import DarkMode from "@/DarkMode";
-// import {
-//   Sheet,
-//   SheetClose,
-//   SheetContent,
-//   SheetFooter,
-//   SheetHeader,
-//   SheetTitle,
-//   SheetTrigger,
-// } from "./ui/sheet";
-// import { Separator } from "@radix-ui/react-dropdown-menu";
-// import { Link, useNavigate } from "react-router-dom";
-// import { useLogoutUserMutation } from "@/features/api/authApi";
-// import { toast } from "sonner";
-// import { useSelector } from "react-redux";
-
-// const Navbar = () => {
-//   const { user } = useSelector((store) => store.auth);
-//   const [logoutUser, { data, isSuccess }] = useLogoutUserMutation();
-//   const navigate = useNavigate();
-//   const logoutHandler = async () => {
-//     await logoutUser();
-//   };
-
-//   useEffect(() => {
-//     if (isSuccess) {
-//       toast.success(data?.message || "User log out.");
-//       navigate("/login");
-//     }
-//   }, [isSuccess]);
-
-//   return (
-//     <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
-//       {/* Desktop */}
-//       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
-//         <div className="flex items-center gap-2">
-          
-//           <Link to="/">
-//             <h1 className="hidden md:block font-extrabold text-2xl">
-//              RainOfProgramming
-//             </h1>
-//           </Link>
-//         </div>
-//         {/* User icons and dark mode icon  */}
-//         <div className="flex items-center gap-8">
-//           {user ? (
-//             <DropdownMenu>
-//               <DropdownMenuTrigger asChild>
-//                 <Avatar>
-//                   <AvatarImage
-//                     src={user?.photoURL || "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" }
-//                     alt="@shadcn"
-//                   />
-//                   <AvatarFallback>PR</AvatarFallback>
-//                 </Avatar>
-//               </DropdownMenuTrigger>
-//               <DropdownMenuContent className="w-56">
-//                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
-//                 <DropdownMenuSeparator />
-//                 <DropdownMenuGroup>
-//                   <DropdownMenuItem>
-//                     <Link to="my-learning">My learning</Link>
-//                   </DropdownMenuItem>
-//                   <DropdownMenuItem>
-//                     {" "}
-//                     <Link to="profile">Edit Profile</Link>{" "}
-//                   </DropdownMenuItem>
-//                   <DropdownMenuItem onClick={logoutHandler}>
-//                     Log out
-//                   </DropdownMenuItem>
-//                 </DropdownMenuGroup>
-//                 {user?.role === "instructor" && (
-//                   <>
-//                     <DropdownMenuSeparator />
-//                     <DropdownMenuItem><Link to="/admin/dashboard">Dashboard</Link></DropdownMenuItem>
-//                   </>
-//                 )}
-//               </DropdownMenuContent>
-//             </DropdownMenu>
-//           ) : (
-//             <div className="flex items-center gap-2">
-//               <Button variant="outline" onClick={() => navigate("/login")}>
-//                 Login
-//               </Button>
-//               <Button onClick={() => navigate("/login")}>Signup</Button>
-//             </div>
-//           )}
-//           <DarkMode />
-//         </div>
-//       </div>
-//       {/* Mobile device  */}
-//       <div className="flex md:hidden items-center justify-between px-4 h-full">
-        
-//         <Link to="/">
-//             <h1 className="font-extrabold text-2xl">
-//              RainOfProgramming
-//             </h1>
-//           </Link>
-//         <MobileNavbar user={user}/>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-// const MobileNavbar = ({user}) => {
-//   const navigate = useNavigate();
-  
-//   return (
-//     <Sheet>
-//       <SheetTrigger asChild>
-//         <Button
-//           size="icon"
-//           className="rounded-full hover:bg-gray-200"
-//           variant="outline"
-//         >
-//           <Menu />
-//         </Button>
-//       </SheetTrigger>
-//       <SheetContent className="flex flex-col">
-//         <SheetHeader className="flex flex-row items-center justify-between mt-2">
-//           <SheetTitle> <Link to="/">RainOfProgramming</Link></SheetTitle>
-//           <DarkMode />
-//         </SheetHeader>
-//         <Separator className="mr-2" />
-//         <nav className="flex flex-col space-y-4">
-//           <Link to="/my-learning">My Learning</Link>
-//           <Link to="/profile">Edit Profile</Link>
-//           <Link to="/login">Sign up</Link>
-//           <Link onClick={logoutHandler}>log out</Link>
-      
-//         </nav>
-//         {user?.role === "instructor" && (
-//           <SheetFooter>
-//             <SheetClose asChild>
-//               <Button type="submit" onClick={()=> navigate("/admin/dashboard")}>Dashboard</Button>
-//             </SheetClose>
-//           </SheetFooter>
-//         )}
-//       </SheetContent>
-//     </Sheet>
-//   );
-// };
-
-
-
-import { Menu, School } from "lucide-react";
+import { Menu } from "lucide-react";
 import React, { useEffect } from "react";
 import {
   DropdownMenu,
@@ -193,13 +32,14 @@ const Navbar = () => {
   const { user } = useSelector((store) => store.auth);
   const [logoutUser, { data, isSuccess }] = useLogoutUserMutation();
   const navigate = useNavigate();
+
   const logoutHandler = async () => {
     await logoutUser();
   };
 
   useEffect(() => {
     if (isSuccess) {
-      toast.success(data?.message || "User log out.");
+      toast.success(data?.message || "User logged out.");
       navigate("/login");
     }
   }, [isSuccess]);
@@ -209,24 +49,26 @@ const Navbar = () => {
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
-          <School size={"30"} />
           <Link to="/">
             <h1 className="hidden md:block font-extrabold text-2xl">
-              E-Learning
+              RainOfProgramming
             </h1>
           </Link>
         </div>
-        {/* User icons and dark mode icon  */}
+        {/* User icons and dark mode icon */}
         <div className="flex items-center gap-8">
           {user ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Avatar>
                   <AvatarImage
-                    src={user?.photoUrl || "https://github.com/shadcn.png"}
+                    src={
+                      user?.photoURL ||
+                      "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+                    }
                     alt="@shadcn"
                   />
-                  <AvatarFallback>CN</AvatarFallback>
+                  <AvatarFallback>PR</AvatarFallback>
                 </Avatar>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
@@ -237,8 +79,7 @@ const Navbar = () => {
                     <Link to="my-learning">My learning</Link>
                   </DropdownMenuItem>
                   <DropdownMenuItem>
-                    {" "}
-                    <Link to="profile">Edit Profile</Link>{" "}
+                    <Link to="profile">Edit Profile</Link>
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={logoutHandler}>
                     Log out
@@ -247,7 +88,9 @@ const Navbar = () => {
                 {user?.role === "instructor" && (
                   <>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem><Link to="/admin/dashboard">Dashboard</Link></DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Link to="/admin/dashboard">Dashboard</Link>
+                    </DropdownMenuItem>
                   </>
                 )}
               </DropdownMenuContent>
@@ -263,10 +106,12 @@ const Navbar = () => {
           <DarkMode />
         </div>
       </div>
-      {/* Mobile device  */}
+      {/* Mobile device */}
       <div className="flex md:hidden items-center justify-between px-4 h-full">
-        <h1 className="font-extrabold text-2xl">E-learning</h1>
-        <MobileNavbar user={user}/>
+        <Link to="/">
+          <h1 className="font-extrabold text-2xl">RainOfProgramming</h1>
+        </Link>
+        <MobileNavbar user={user} logoutHandler={logoutHandler} />
       </div>
     </div>
   );
@@ -274,9 +119,15 @@ const Navbar = () => {
 
 export default Navbar;
 
-const MobileNavbar = ({user}) => {
+const MobileNavbar = ({ user, logoutHandler }) => {
   const navigate = useNavigate();
-  
+
+  const handleLogout = async () => {
+    await logoutHandler();
+    toast.success("User logged out.");
+    navigate("/login");
+  };
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -290,19 +141,44 @@ const MobileNavbar = ({user}) => {
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <SheetHeader className="flex flex-row items-center justify-between mt-2">
-          <SheetTitle> <Link to="/">E-Learning</Link></SheetTitle>
+          <SheetTitle>
+            <Link to="/">RainOfProgramming</Link>
+          </SheetTitle>
           <DarkMode />
         </SheetHeader>
         <Separator className="mr-2" />
         <nav className="flex flex-col space-y-4">
-          <Link to="/my-learning">My Learning</Link>
-          <Link to="/profile">Edit Profile</Link>
-          <p>Log out</p>
+          <SheetClose asChild>
+            <Link to="/my-learning">My Learning</Link>
+          </SheetClose>
+          <SheetClose asChild>
+            <Link to="/profile">Edit Profile</Link>
+          </SheetClose>
+          <SheetClose asChild>
+            <Link to="/login">Sign up</Link>
+          </SheetClose>
+          {user && (
+            <SheetClose asChild>
+              <button
+                variant="ghost"
+                className="text-left w-full"
+                onClick={handleLogout}
+              >
+                Log out
+              </button>
+            </SheetClose>
+          )}
         </nav>
         {user?.role === "instructor" && (
           <SheetFooter>
             <SheetClose asChild>
-              <Button type="submit" onClick={()=> navigate("/admin/dashboard")}>Dashboard</Button>
+              <Button
+                type="button"
+                className="w-full text-white bg-black dark:bg-white dark:text-black hover:bg-gray-700 dark:hover:bg-gray-300"
+                onClick={() => navigate("/admin/dashboard")}
+              >
+                Dashboard
+              </Button>
             </SheetClose>
           </SheetFooter>
         )}
